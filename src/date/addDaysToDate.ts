5176cf02389fd31fbd730fdd9662a630e65c2c2a
giseles <17761608@qq.com>
2021-06-03 10:51:18 +0800

    fix:npm优化

--- src/date/addDaysToDate.ts
+++ src/date/addDaysToDate.ts
@@ -1,8 +1,9 @@
-const addDaysToDate = (date: any = null, n: number) => {
+/**日期-前后天数  
+ * addDaysToDate('2020-10-15', 10);   // '2020-10-25'
+ * addDaysToDate('2020-10-15', -10);  // '2020-10-05'
+ * */
+export const addDaysToDate = (date: any = null, n: number) => {
     const d = new Date(date);
     d.setDate(d.getDate() + n);
     return d.toISOString().split('T')[0];
 };
-
-//   addDaysToDate('2020-10-15', 10); // '2020-10-25'
-//   addDaysToDate('2020-10-15', -10); // '2020-10-05'
--- src/date/index.ts
+++ src/date/index.ts
@@ -1,9 +1,11 @@
 import { dateToTimestamp } from './dateToTimestamp';
 import { timestampToDate } from './timestampToDate';
 import { timestampToDateNoTime } from './timestampToDateNoTime';
+import { addDaysToDate } from './addDaysToDate';
 
 export default {
     dateToTimestamp,
     timestampToDate,
-    timestampToDateNoTime
+    timestampToDateNoTime,
+    addDaysToDate
 };
