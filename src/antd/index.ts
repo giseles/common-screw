70aad7e5f295ebff3ed8169ecefd814a559e11de
gisele <17761608@qq.com>
2021-12-30 15:24:01 +0800

    fix:注释优化

--- src/antd/index.ts
+++ src/antd/index.ts
@@ -5,7 +5,7 @@ import { antdMessage } from "./antdMessage"
  * @name antd 通用方法
  * @example
  * antdMessage    //统一返回数据提示
- * antdConfirm    //统一返回数据弹窗
+ * antdConfirm    //统一返回信息提示框
  */
 export const CSAntd = {
   antdConfirm,
